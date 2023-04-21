a8e01dc19891e8ce73a165c4a9c9656f200a1bdb
DIR\alba.lima.garcia <albalimav70@hotmail.com>
2023-04-21 09:39:45 +0200

    creación de proyecto y estructura

---
+++ src/main/java/com/fpdual/javaweb/persistence/connector/MySQLConstants.java
@@ -0,0 +1,16 @@
+package com.fpdual.javaweb.persistence.connector;
+
+public class MySQLConstants {
+    public static final String PASSWD = "jdbc.mysql.passwd";
+    public static final String USER = "jdbc.mysql.user";
+    public static final String DRIVER = "jdbc.mysql.driver";
+    public static final String URL_PREFIX = "jdbc.mysql.url.prefix";
+    public static final String URL_HOST = "jdbc.mysql.url.host";
+    public static final String URL_PORT = "jdbc.mysql.url.port";
+    public static final String URL_SCHEMA = "jdbc.mysql.url.schema";
+    public static final String URL_SSL = "jdbc.mysql.url.ssl";
+    public static final String ALLOW_PUBLIC_KEY_RETRIEVAL = "jdbc.mysql.url.allowPublicKeyRetrieval";
+    public static final String USE_JDBC_COMPLIANT_TIMEZONE_SHIFT = "jdbc.mysql.url.useJDBCCompliantTimezoneShift";
+    public static final String USE_LEGACY_DATE_TIME_CODE = "jdbc.mysql.url.useLegacyDatetimeCode";
+    public static final String SERVER_TIMEZONE = "jdbc.mysql.url.serverTimezone";
+}
