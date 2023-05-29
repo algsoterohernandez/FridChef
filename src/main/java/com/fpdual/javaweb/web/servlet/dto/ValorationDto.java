07541255720f662bf10b135d0a1c4a14f7b32bdc
DIR\alba.lima.garcia <albalimav70@hotmail.com>
2023-05-29 11:41:43 +0200

    creacion ValorationDto

---
+++ src/main/java/com/fpdual/javaweb/web/servlet/dto/ValorationDto.java
@@ -0,0 +1,9 @@
+package com.fpdual.javaweb.web.servlet.dto;
+
+public class ValorationDto {
+    private int id;
+    private int idRecipe;
+    private int idUser;
+    private String coment;
+    private double valoration;
+}
